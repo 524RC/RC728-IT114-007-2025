cb3795e9151898b9af70e8a43b9d5230ec2c7f2b
524RC <Rchung524@gmail.com>
2025-09-29 23:28:19 -0400

    solved problem 4 with comments

--- M2/Problem4.java
+++ M2/Problem4.java
@@ -31,7 +31,41 @@ public class Problem4 extends BaseClass {
         
         for(int i = 0; i <arr.length; i++){
             // Start Solution Edits
-            
+            //Rc728 9/29/2025
+            //first noSpecialChar repalces all the special characters that aren't a-z, A-Z, or 0-9 with "" then noSpiecialsTrimmed
+            //will trim the edges of the string so " hello world " would become "hello world"
+            //noSpecialTrimmednoWS removes all the whitespaces with " " and chars splits and creates an array for each character 
+            //first it sets capitalize to true so the first character would be capital and when it sees a " " it will make the character
+            //after that capitalized so all the words start with capital letters
+            //after everything has been tweaked, it all gets put into sentenceProcessed to join all the words together
+            //then everything gets assigned to placeholderForModifiedPhrase
+            String[] chars = {};
+            for(int x = 0; x < arr.length; x++){
+               String noSpecialChar = arr[i].replaceAll("[^a-zA-Z0-9 ]", "");
+               String noSpecialsTrimmed = noSpecialChar.trim();
+               String noSpecialsTrimmednoWS = noSpecialsTrimmed.replaceAll("\\s+", " ");
+               chars = noSpecialsTrimmednoWS.split("");
+
+               boolean capitalize = true;
+               for(int y = 0; y < noSpecialsTrimmednoWS.length(); y++){
+                    if(chars[y].equals(" ")){
+                        capitalize = true;
+                        if(y + 1 < chars.length){
+                            chars[y+1] = chars[y+1].toUpperCase();
+                        }
+                    }else{
+                        if (capitalize){
+                            chars[y] = chars[y].toUpperCase();
+
+                        }else{
+                            chars[y] = chars[y].toLowerCase();
+                        }
+                    } 
+                    capitalize = false;
+               }
+            }
+            String sentenceProcessed = String.join("", chars);
+            placeholderForModifiedPhrase = sentenceProcessed;
              // End Solution Edits
             System.out.println(String.format("Index[%d] \"%s\" | Middle: \"%s\"",i, placeholderForModifiedPhrase, placeholderForMiddleCharacters));
         }
