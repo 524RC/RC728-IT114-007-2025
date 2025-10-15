8ba29cc4249b153bf595040926e73c50d8ee48ba
524RC <Rchung524@gmail.com>
2025-10-15 16:40:49 -0400

    restarted Problem 1

--- M3/CommandLineCalculator.java
+++ M3/CommandLineCalculator.java
@@ -1,5 +1,6 @@
 package M3;
 
+
 /*
 Challenge 1: Command-Line Calculator
 ------------------------------------
@@ -11,11 +12,14 @@ Challenge 1: Command-Line Calculator
 - Capture 5 variations of tests
 */
 
+
 public class CommandLineCalculator extends BaseClass {
     private static String ucid = "Rc728"; // <-- change to your ucid
 
+
     public static void main(String[] args) {
         printHeader(ucid, 1, "Objective: Implement a calculator using command-line arguments.");
+
 
         if (args.length != 3) {
             System.out.println("Usage: java M3.CommandLineCalculator <num1> <operator> <num2>");
@@ -23,13 +27,15 @@ public class CommandLineCalculator extends BaseClass {
             return;
         }
 
+
         try {
             System.out.println("Calculating result...");
             // extract the equation (format is <num1> <operator> <num2>)
             //Rc728 10/14/2025
             String num1String = args[0];
             String operator = args[1];
-            String num2String = args[2];
+            String num2String = args[2];c
+
 
             double num1 = Double.parseDouble(num1String);
             double num2 = Double.parseDouble(num2String);
@@ -41,22 +47,26 @@ public class CommandLineCalculator extends BaseClass {
                 sum = num1 - num2;
             }else{
                 System.out.print("unsupported operator: " + operator);
-                printFooter(num2String, 1);
+                printFooter(ucid, 1);
                 return;
             }
 
-            
+
+           
             // check the type of each number and choose appropriate parsing
-            
+           
             // generate the equation result (Important: ensure decimals display as the
             // longest decimal passed)
             // i.e., 0.1 + 0.2 would show as one decimal place (0.3), 0.11 + 0.2 would shows
             // as two (0.31), etc
-
+       
         } catch (Exception e) {
             System.out.println("Invalid input. Please ensure correct format and valid numbers.");
         }
-
+       
         printFooter(ucid, 1);
+       
     }
 }
+
+
